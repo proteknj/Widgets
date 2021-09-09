8607cdaa272296ed695a6586d69141420269d8aa
vinit kumar <vinitbharadwaj86@gmail.com>
2021-09-09 11:42:18 -0400

    commit chnages

--- src/App.js
+++ src/App.js
@@ -34,13 +34,19 @@ const options=[
 ]
 export default () => {
     const [selected,setSelected]=useState(options[0]);
+    const [showDropdown,setShowDropdown]=useState(true);
     return (
        <div>
-           <Dropdown 
+           <button onClick={()=>setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
+           
+          { showDropdown?
+          ( <Dropdown 
            selected={selected} 
            onSelectedChange={setSelected} 
-           options={options}/>
-           <Search/>
+           options={options}
+           />) : null
+        }
+           
        </div>
 
     );
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef} from 'react';
 
 const Dropdown=({options,selected,onSelectedChange})=>{
     const [open,setOpen] = useState(false);
+    const ref=useRef();
+    useEffect(()=>{
+        const onBodyClick=(event)=>{
+            if(ref.current.contains(event.target)){
+                return;
+            }
+                setOpen(false)  
+              }
+        document.body.addEventListener('click',onBodyClick,{capture:true});
+        return ()=>{
+           document.body.removeEventListener('click',onBodyClick,{capture:true})
+        }
+    },[])
     const renderedOptions=options.map((option)=>{
         if(option.value === selected.value){
            return null;
@@ -15,8 +28,9 @@ const Dropdown=({options,selected,onSelectedChange})=>{
             </div>   
         )
     })
+    
     return (
-        <div className='ui form'>
+        <div className='ui form' ref={ref}>
           <div className='field'>
              <label className='label'>Select a Color</label>
              <div onClick={()=>setOpen(!open) } 
